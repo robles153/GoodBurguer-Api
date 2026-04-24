c584987b4a6846614bc100f555382b5b2c95a486
robes153 <marcoskim153@gmail.com>
2026-04-24 14:25:18 -0300

    feat: Implementando classe de configuração de injeção de dependencia do Application

---
+++ GoodBurguer/GoodBurguer.API/Configurations/DependencyInjectionApplicationConfig.cs
@@ -0,0 +1,22 @@
+﻿using GoodBurguer.GoodBurguer.Application.Pedidos.CriarPedido;
+using GoodBurguer.GoodBurguer.Domain.Strategies;
+
+namespace GoodBurguer.GoodBurguer.API.Configurations
+{
+    public static class DependencyInjectionApplicationConfig
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            // Service
+            services.AddScoped<CriarPedidoService>();
+
+            // Strategies 
+            services.AddScoped<IDescontoStrategy, DescontoComboCompleto>();
+            services.AddScoped<IDescontoStrategy, DescontoSanduicheBebida>();
+            services.AddScoped<IDescontoStrategy, DescontoSanduicheAcompanhamento>();
+            services.AddScoped<IDescontoStrategy, SemDescontoStrategy>();
+
+            return services;
+        }
+    }
+}
